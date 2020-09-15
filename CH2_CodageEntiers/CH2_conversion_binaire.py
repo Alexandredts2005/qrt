72dd268e7fea5155abf86c8af87bb0ecd481bb56
edaldegan <edaldegan@gitlab.com>
2020-09-15 14:31:14 +0200

    Mise en ligne du CH2

---
+++ CH2_CodageEntiers/CH2_conversion_binaire.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Conversion d'un nombre binaire
+
+print("Saisir les 4 bits 0 ou 1, de gauche à droite : ")
+
+bit1 = int(input("BIT 1 : "))
+bit2 = int(input("BIT 2 : "))
+bit3 = int(input("BIT 3 : "))
+bit4 = int(input("BIT 4 : "))
+    
+ ## A COMPLETER ##
+
