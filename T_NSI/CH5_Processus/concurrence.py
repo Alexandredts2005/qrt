b54a892caec5085dd8564a423322c727eeba6c2b
edaldegan <edaldegan@gitlab.com>
2023-01-10 15:35:27 +0100

    MàJ concurrence.py

--- T_NSI/CH5_Processus/concurrence.py
+++ T_NSI/CH5_Processus/concurrence.py
@@ -2,14 +2,11 @@ import threading
 
 COMPTEUR = 0
 
-verrou = threading.Lock()
 
 def increment():
-    verrou.acquire()
     global COMPTEUR
     for val in range(100000):
         COMPTEUR = COMPTEUR + 1
-    verrou.release()
 mes_threads = []
 
 for n in range(4):
