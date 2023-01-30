ca356aed76bdb940949605781154db2465c83293
edaldegan <edaldegan@gitlab.com>
2023-01-30 16:15:35 +0100

    Implémentation AB

---
+++ T_NSI/CH7_Arbres/TP implementation_arbre_binaire/classeFile.py
@@ -0,0 +1,40 @@
+﻿class File:
+   """
+   Représentation d'une file, implémentée grâce aux listes python.
+   """
+
+   def __init__(self):
+      """
+      Crée une file vide
+      """
+      self.file = []
+
+   def est_vide(self):
+      """
+      Renvoie True ssi la file est vide.
+      """
+      return len(self.file) == 0
+
+   def taille(self):
+      """
+      Renvoie la taille de la file (en nombre d'éléments emfilés)
+      """
+      return len(self.file)
+
+   def enfiler(self, valeur):
+      """
+      Enfile un nouvel élément.
+      """
+      self.file.append(valeur)
+
+   def defiler(self):
+      """
+      Défile et renvoie la valeur de l'élément placé premier
+      dans la file (le plus ancien).
+
+      Déclenche une erreur si la file est vide.
+      """
+      if self.est_vide():
+         raise IndexError("Défiler sur une file vide")
+      else:
+         return self.file.pop(0)
---
+++ T_NSI/CH7_Arbres/TP implementation_arbre_binaire/classePile.py
@@ -0,0 +1,41 @@
+﻿class Pile:
+   """
+   Représentation d'une pile, implémentée grâce aux listes python.
+   """
+
+   def __init__(self):
+      """
+      Crée une pile vide
+      """
+      self.pile = []
+
+   def est_vide(self):
+      """
+      Renvoie True ssi la pile est vide.
+      """
+      return len(self.pile) == 0
+
+   def taille(self):
+      """
+      Renvoie la taille de la pile (en nombre d'éléments empilés)
+      """
+      return len(self.pile)
+
+   def empiler(self, valeur):
+      """
+      Empile un nouvel élément
+      """
+      self.pile.append(valeur)
+
+   def depiler(self):
+      """
+      Dépile et renvoie la valeur de l'élément placé au sommet
+      Déclenche une erreur si la pile est vide.
+      """
+      if self.est_vide():
+         raise IndexError("Dépiler sur une pile vide")
+      else:
+         return self.pile.pop()
+
+pile = Pile()
+pile.taille()
