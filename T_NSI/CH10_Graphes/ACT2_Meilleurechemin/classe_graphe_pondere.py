bb6dd35360702259bd08e01eb7e1d12fd098ed79
edaldegan <edaldegan@gitlab.com>
2023-04-05 09:03:01 +0200

    graphes

--- T_NSI/CH10_Graphes/ACT2_Meilleurechemin/classe_graphe_pondere.py
+++ T_NSI/CH10_Graphes/ACT2_Meilleurechemin/classe_graphe_pondere.py
@@ -1,5 +1,3 @@
-
-
 class Graphe_P:
     '''Graphe pondéré représenté par un dictionnaire d'adjacence'''
 
@@ -8,22 +6,23 @@ class Graphe_P:
 
     def affiche(self):
         for k in self.adj:
-            print (k,self.adj[k])
+            print(k, self.adj[k])
 
     def ajouter_sommet(self,s):
         if s not in self.adj:
-            self.adj[s]={}
+            self.adj[s] = {}
 
     def ajouter_arc(self, s1, s2, p):
         """ajoute l'arc de s1 à s2 en lui affectant le poids p"""
         self.ajouter_sommet(s1)
 
         # à compléter
-
+       
     def arc(self,s1,s2):
         """ renvoie le poids de l'arc de s1 à s2"""
         for s in self.adj[s1].keys():
        # à compléter
+  
 
     def sommets(self):
         s=[]
@@ -34,7 +33,7 @@ class Graphe_P:
     def voisins(self, s):
         """ renvoie la liste des voisins du sommet s"""
         # à compléter
-
+     
 
 
 
